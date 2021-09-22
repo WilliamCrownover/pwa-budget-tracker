5bd54d5115629d1485d566d71d5689106d3ad0a1
William Crownover <williamcrownover1@gmail.com>
2021-09-22 01:12:47 -0700

    Added file paths to cache list in service worker

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const FILES_TO_CACHE = [
+	'/',
+	'/index.html',
+	'/index.js',
+	'/db.js',
+	'/manifest.webmanifest',
+	'/styles.css',
+	'icons//icon-192x192.png',
+	'icons//icon-512x512.png'
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+

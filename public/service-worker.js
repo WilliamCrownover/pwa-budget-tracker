84cab5d5232461aa93c8947b7b1776253bb5c7b2
William Crownover <williamcrownover1@gmail.com>
2021-09-22 23:12:35 -0700

    Service-worker install event

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// Cache file paths
 const FILES_TO_CACHE = [
 	'/',
 	'/index.html',
@@ -5,10 +6,21 @@ const FILES_TO_CACHE = [
 	'/db.js',
 	'/manifest.webmanifest',
 	'/styles.css',
-	'icons//icon-192x192.png',
-	'icons//icon-512x512.png'
+	'icons/icon-192x192.png',
+	'icons/icon-512x512.png'
 ];
 
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// Install
+self.addEventListener("install", function (evt) {
+	evt.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			console.log("Your files were pre-cached successfully!");
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+
+	self.skipWaiting();
+});

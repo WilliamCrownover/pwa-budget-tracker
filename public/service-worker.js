40cee94c399ccbf5002ba09a3b8a949befca3e39
William Crownover <williamcrownover1@gmail.com>
2021-09-22 23:33:32 -0700

    Service-worker fetch event and fixed icon paths in cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,11 +6,11 @@ const FILES_TO_CACHE = [
 	'/db.js',
 	'/manifest.webmanifest',
 	'/styles.css',
-	'icons/icon-192x192.png',
-	'icons/icon-512x512.png'
+	'/icons/icon-192x192.png',
+	'/icons/icon-512x512.png'
 ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v3";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // Install
@@ -41,4 +41,37 @@ self.addEventListener("activate", function (evt) {
 	);
 
 	self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", function (evt) {
+	// cache successful requests to the API
+	if (evt.request.url.includes("/api/")) {
+		evt.respondWith(
+			caches.open(DATA_CACHE_NAME).then(cache => {
+				return fetch(evt.request)
+					.then(response => {
+						// If the response was good, clone it and store it in the cache.
+						if (response.status === 200) {
+							cache.put(evt.request.url, response.clone());
+						}
+
+						return response;
+					})
+					.catch(err => {
+						// Network request failed, try to get it from the cache.
+						return cache.match(evt.request);
+					});
+			}).catch(err => console.log(err))
+		);
+
+		return;
+	}
+
+	// if the request is not for the API, serve static assets using "offline-first" approach.
+	evt.respondWith(
+		caches.match(evt.request).then(function (response) {
+			return response || fetch(evt.request);
+		})
+	);
 });
